def0186f2ae32f159124f2669dcffa44d2db1a89
onewayay <onewayay@naver.com>
2026-05-04 19:30:15 +0900

    72강 : 좋아요 결과 반영하기 (낙관적 업데이트)

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@ import type { Database } from "@/database.types";
 export type PostEntity = Database["public"]["Tables"]["post"]["Row"];
 export type ProfileEntity = Database["public"]["Tables"]["profile"]["Row"];
 
-export type Post = PostEntity & { author: ProfileEntity; isLikedL: boolean };
+export type Post = PostEntity & { author: ProfileEntity; isLiked: boolean };
 
 // mutation의 callbacks 타입
 export type UseMutationCallback = {
